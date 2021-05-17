d7b191a5d933eb8a5721daa0e5e72e97561d118a
nivrrex <nivrrex@gmail.com>
2021-05-17 13:45:04 +0800

    Update main.c

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ void loop(int sockfd)
     memset((char *) &out_addr, 0, sizeof(out_addr));
     out_addr.sin_family = AF_INET;
     out_addr.sin_port   = htons(config.dns_port);
-    inet_aton(config.dns, (struct in_addr *)&out_addr.sin_addr.s_addr);
+    inet_aton(config.dns_ip, (struct in_addr *)&out_addr.sin_addr.s_addr);
     out_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (out_socket < 0) error("ERROR opening socket out");
 
