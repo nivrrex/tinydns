47029cb4e266eef233a207c272ca699ac42b829d
nivrrex <nivrrex@gmail.com>
2021-05-19 08:42:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -5,128 +5,14 @@
 #include <netdb.h>
 #define MAX_EVENT_NUM 256
 
-char version[] = "0.4.0";
+char version[] = "0.4.1";
 unsigned char buf[0xFFF];
+struct epoll_event ev, events[MAX_EVENT_NUM];
 
 void error(char *msg) { log_s(msg); perror(msg); exit(1); }
 
-int hostname_to_ip(char *hostname, char *ip, int len)
+void loop(int epfd)
 {
-    int sockfd;
-    struct addrinfo hints;
-struct addrinfo *servinfo;
-struct addrinfo *p;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC;
-    if (getaddrinfo(hostname, NULL, &hints, &servinfo) != 0) return 0;
-
-    for (p = servinfo; p != NULL; p = p->ai_next)
-    {
-        if (p->ai_family == AF_INET6)
-        {
-            struct sockaddr_in6 *serveraddr = (struct sockaddr_in6 *)p->ai_addr;
-            inet_ntop(AF_INET6, (struct in_addr *)&serveraddr->sin6_addr, ip, len);
-            break;
-        }
-        else
-        if (p->ai_family == AF_INET)
-        {
-            struct sockaddr_in *serveraddr = (struct sockaddr_in *)p->ai_addr;
-            inet_ntop(AF_INET, (struct in_addr *)&serveraddr->sin_addr, ip, len);
-        }
-    }
-    freeaddrinfo(servinfo);
-    return 1;
-}
-
-int main(int argc, char **argv)
-{
-    if (argv[1] && 0 == strcmp(argv[1], "--version"))
-    {
-        printf("tinydns %s\nAuthor: CupIvan <mail@cupivan.ru>\nLicense: MIT\n", version);
-        exit(0);
-    }
-
-    if (argv[1] && 0 == strcmp(argv[1], "--help"))
-    {
-        help();
-        exit(0);
-    }
-
-    if (argv[1] && 0 == strcmp(argv[1], "-d"))
-    {
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            if (pid < 0) error("Can't create daemon!");
-            exit(1);
-        }
-        if (pid > 0) exit(0); // exit from current process
-    }
-    else
-        config.debug_level = 1;
-
-    config_load();
-
-    int sock,epfd,nfds,sockfd,rc;
-    // convert domain to IP
-    char buf[0xFF];
-    if (hostname_to_ip(config.server_ip, buf, sizeof(buf)))
-        config.server_ip = buf;
-
-    // is ipv6?
-    int is_ipv6 = 0, j = 0;
-    while (config.server_ip[j]) if (config.server_ip[j++] == ':') { is_ipv6 = 1; break; }
-
-    // create socket
-    sock = socket(is_ipv6 ? AF_INET6 : AF_INET, SOCK_DGRAM, 0);
-    if (sock < 0) error("ERROR opening socket");
-
-    //epoll create and ctl
-	struct epoll_event ev, events[MAX_EVENT_NUM];
-	epfd = epoll_create(MAX_EVENT_NUM);
-    memset(&ev, 0, sizeof(ev));
-    ev.events = EPOLLIN ;
-    ev.data.fd = sock;
-    rc = epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev);
-    if(rc < 0)
-        error("Cannot add socket to epoll!\n");
-
-    /* setsockopt: Handy debugging trick that lets
-    * us rerun the server immediately after we kill it;
-    * otherwise we have to wait about 20 secs.
-    * Eliminates "ERROR on binding: Address already in use" error.
-    */
-    int optval = 1;
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(optval));
-
-    // bind
-    if (is_ipv6)
-    {
-        struct sockaddr_in6 serveraddr;  /* server's addr */
-        memset((char *) &serveraddr, 0, sizeof(serveraddr));
-        serveraddr.sin6_family = AF_INET6;
-        serveraddr.sin6_port   = htons(config.server_port);
-        inet_pton(AF_INET6, config.server_ip, (struct in_addr *)&serveraddr.sin6_addr.s6_addr);
-        if (bind(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
-            error("ERROR on binding ipv6");
-    }
-    else
-    {
-        struct sockaddr_in serveraddr;  /* server's addr */
-        memset((char *) &serveraddr, 0, sizeof(serveraddr));
-        serveraddr.sin_family = AF_INET;
-        serveraddr.sin_port   = htons(config.server_port);
-        inet_aton(config.server_ip, (struct in_addr *)&serveraddr.sin_addr.s_addr);
-        if (bind(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
-            error("ERROR on binding ipv4");
-    }
-
-    char s[0xFF];
-    sprintf(s, "already bind on %s:%d", config.server_ip, config.server_port);
-
-    log_s(s);
     int16_t   i, n;
     uint16_t  id;
     uint16_t  *ans = NULL;
@@ -145,6 +31,7 @@ int main(int argc, char **argv)
     out_socket = socket(AF_INET, SOCK_DGRAM, 0);
     if (out_socket < 0) error("ERROR opening socket out");
 
+    int nfds,sockfd,rc;
     while (1)
     {
         nfds = epoll_wait(epfd, events, MAX_EVENT_NUM, 500);
@@ -227,4 +114,128 @@ int main(int argc, char **argv)
                 error("Cannot mod socket to epoll!\n");
         }
     }
+}
+
+int hostname_to_ip(char *hostname, char *ip, int len)
+{
+    int sockfd;
+    struct addrinfo hints;
+    struct addrinfo *servinfo;
+    struct addrinfo *p;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_UNSPEC;
+    if (getaddrinfo(hostname, NULL, &hints, &servinfo) != 0) return 0;
+
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if (p->ai_family == AF_INET6)
+        {
+            struct sockaddr_in6 *serveraddr = (struct sockaddr_in6 *)p->ai_addr;
+            inet_ntop(AF_INET6, (struct in_addr *)&serveraddr->sin6_addr, ip, len);
+            break;
+        }
+        else
+        if (p->ai_family == AF_INET)
+        {
+            struct sockaddr_in *serveraddr = (struct sockaddr_in *)p->ai_addr;
+            inet_ntop(AF_INET, (struct in_addr *)&serveraddr->sin_addr, ip, len);
+        }
+    }
+    freeaddrinfo(servinfo);
+    return 1;
+}
+
+int server_init()
+{
+    int sock,epfd,rc;
+    // convert domain to IP
+    char buf[0xFF];
+    if (hostname_to_ip(config.server_ip, buf, sizeof(buf)))
+        config.server_ip = buf;
+
+    // is ipv6?
+    int is_ipv6 = 0, j = 0;
+    while (config.server_ip[j]) if (config.server_ip[j++] == ':') { is_ipv6 = 1; break; }
+
+    // create socket
+    sock = socket(is_ipv6 ? AF_INET6 : AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) error("ERROR opening socket");
+
+    //epoll create and ctl
+	epfd = epoll_create(MAX_EVENT_NUM);
+    memset(&ev, 0, sizeof(ev));
+    ev.events = EPOLLIN ;
+    ev.data.fd = sock;
+    rc = epoll_ctl(epfd, EPOLL_CTL_ADD, sock, &ev);
+    if(rc < 0)
+        error("Cannot add socket to epoll!\n");
+
+    /* setsockopt: Handy debugging trick that lets
+    * us rerun the server immediately after we kill it;
+    * otherwise we have to wait about 20 secs.
+    * Eliminates "ERROR on binding: Address already in use" error.
+    */
+    int optval = 1;
+    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(optval));
+
+    // bind
+    if (is_ipv6)
+    {
+        struct sockaddr_in6 serveraddr;  /* server's addr */
+        memset((char *) &serveraddr, 0, sizeof(serveraddr));
+        serveraddr.sin6_family = AF_INET6;
+        serveraddr.sin6_port   = htons(config.server_port);
+        inet_pton(AF_INET6, config.server_ip, (struct in_addr *)&serveraddr.sin6_addr.s6_addr);
+        if (bind(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
+            error("ERROR on binding ipv6");
+    }
+    else
+    {
+        struct sockaddr_in serveraddr;  /* server's addr */
+        memset((char *) &serveraddr, 0, sizeof(serveraddr));
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_port   = htons(config.server_port);
+        inet_aton(config.server_ip, (struct in_addr *)&serveraddr.sin_addr.s_addr);
+        if (bind(sock, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
+            error("ERROR on binding ipv4");
+    }
+
+    char s[0xFF];
+    sprintf(s, "already bind on %s:%d", config.server_ip, config.server_port);
+    log_s(s);
+
+    return epfd;
+}
+
+int main(int argc, char **argv)
+{
+    if (argv[1] && 0 == strcmp(argv[1], "--version"))
+    {
+        printf("tinydns %s\nAuthor: CupIvan <mail@cupivan.ru>\nLicense: MIT\n", version);
+        exit(0);
+    }
+
+    if (argv[1] && 0 == strcmp(argv[1], "--help"))
+    {
+        help();
+        exit(0);
+    }
+
+    if (argv[1] && 0 == strcmp(argv[1], "-d"))
+    {
+        pid_t pid = fork();
+        if (pid < 0)
+        {
+            if (pid < 0) error("Can't create daemon!");
+            exit(1);
+        }
+        if (pid > 0) exit(0); // exit from current process
+    }
+    else
+        config.debug_level = 1;
+
+    config_load();
+    int epfd = server_init();
+    loop(epfd);
 }
