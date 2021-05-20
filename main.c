fe01c8404cd093f10c2cf5730bc7009b89f233ab
nivrrex <nivrrex@gmail.com>
2021-05-20 12:33:19 +0800

    Update main.c

--- main.c
+++ main.c
@@ -230,7 +230,7 @@ int main(int argc, char **argv)
 {
     if (argv[1] && 0 == strcmp(argv[1], "--version"))
     {
-        printf("tinydns %s\nAuthor: CupIvan <mail@cupivan.ru>\nEditor: Nivrrex <nivrrex@gmail.com>\nLicense: MIT\n", version);
+        printf("tinydns %s\nCompile Time: %s %s\nAuthor: CupIvan <mail@cupivan.ru>\nEditor: Nivrrex <nivrrex@gmail.com>\nLicense: MIT\n", version,__DATE__,__TIME__);
         exit(0);
     }
 
